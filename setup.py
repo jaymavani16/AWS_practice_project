783132494591a758b23704f11f880ae31f620a82
Jay Mavani <jaymavanime39@gmail.com>
2026-07-08 11:46:02 +0530

    This is first commit for all the primary changes and all the setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a project for practicing AWS and connecting pipeline and versioning data and connecting data with AWS.',
+    author='Jay Mavani',
+    license='MIT',
+)
